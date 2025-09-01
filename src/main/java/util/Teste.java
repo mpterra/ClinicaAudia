4be9a571cf7ceebecad8c5d587fcc3cbf9e92c1d
mpterra <mauriciopterra@gmail.com>
2025-09-01 12:59:22 -0300

    Testes de atualzação de senha

--- src/main/java/util/Teste.java
+++ src/main/java/util/Teste.java
@@ -10,33 +10,17 @@ import model.Usuario;
 
 public class Teste {
 
-    public static void main(String[] args) {
-        UsuarioController uc = new UsuarioController();
+    public static void main(String[] args) throws SQLException, LoginDuplicadoException {
 
         // Testar cadastro
+        UsuarioController uc = new UsuarioController();
         Usuario user = new Usuario();
-        user.setLogin("admin");
-        user.setSenha("cba321");
-        user.setTipo("ADMIN");
+		user = uc.buscarPorLogin("admin");
+        System.out.println(user);
+        
+        user.setSenha("54321");
+        
+        uc.atualizar(user);
 
-        try {
-            Usuario salvo = uc.salvar(user);
-            JOptionPane.showMessageDialog(null, "Usuário cadastrado!");
-            System.out.println("Usuário cadastrado: " + salvo);
-
-        } catch (LoginDuplicadoException e) {
-        	JOptionPane.showMessageDialog(null, "Erro: " + e.getMessage());
-            System.out.println("Erro: " + e.getMessage());
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
-
-        // Testar login
-        try {
-            boolean conectou = uc.login("admin", "cba321");
-            System.out.println("Conectou? " + conectou);
-        } catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
